c3aa604438f026e099f6a8ae73dd5bf2f009a5a3
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-10-03 18:25:21 +0600

    some change

--- programming_hero_task_1.sql
+++ programming_hero_task_1.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ostad_programming_hero_task_1`
+-- Database: `programming_hero_task_1`
 --
 
 -- --------------------------------------------------------
